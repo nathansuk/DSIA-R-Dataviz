f509a521882a53cff382d961d1f2cff99b774251
Nathan SUK <46647779+nathansuk@users.noreply.github.com>
2023-12-12 15:34:02 +0100

    packages fix again

--- app.R
+++ app.R
@@ -1,4 +1,10 @@
-install.packages(c("shiny", "leaflet", "dplyr", "ggplot2", "sf", "tidyverse"))
+packages <- c("shiny", "leaflet", "dplyr", "ggplot2", "sf", "tidyverse")
+
+new_packages <- packages[!(packages %in% installed.packages()[,"Package"])]
+if(length(new_packages)) install.packages(new_packages)
+
+lapply(packages, library, character.only = TRUE)
+
 library(shiny)
 library(leaflet)
 
